85525697ee7e2460aea4016a32a515c73c47c2bc
roverkalisl <roverkalisl@gmail.com>
2026-08-27 14:38:36 +0530

    Add Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1", "teahousevilla.onrender.com"])
 
 
 INSTALLED_APPS = [
