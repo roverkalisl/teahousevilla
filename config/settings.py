dd9dc2a8856f148e9e7ae4e60cc31c171b23cc81
roverkalisl <roverkalisl@gmail.com>
2026-08-27 13:47:02 +0530

    Update Tea House Villa

--- config/settings.py
+++ config/settings.py
@@ -60,10 +60,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db(
+        "DATABASE_URL",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    )
 }
 
 
